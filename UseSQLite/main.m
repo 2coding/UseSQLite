c74db9ca93b50e4812ceb602265197d5f30edd25
2coding <CodingApe@outlook.com>
2016-01-26 21:29:59 +0800

    add files

---
+++ UseSQLite/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UseSQLite
+//
+//  Created by hesy on 16/1/26.
+//  Copyright © 2016年 2Coding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
